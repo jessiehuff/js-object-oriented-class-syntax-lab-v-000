81fab0c4cb88634bb3eda88d3376bb0c9e532e51
Jessie Huff <huffjd1@gmail.com>
2018-03-08 16:12:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ class BoardMember {
     this.name = name; 
     this.homeState = homeState;
     this.training = training; 
-  };
+  }
   
   veto(){
     returns "No, I must disagree";
-  };
+  }
   
   approve(){
     returns `You can do that!`;
-  };
+  }
   doCharity(){
     returns `I like to help people.`;
-  };
+  }
   releasePressStatement(){
     returns `You will see great things from Scuber.`;
-  };
+  }
   sayHi(){
     returns `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`;
-  };
+  }
 }
 
 class Ceo extends BoardMember {
